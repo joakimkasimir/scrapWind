a1412360b7f7bc0b765701129e60f4c896b51fc4
Joakim Kasimir <joakim.kasimir@gmail.com>
2017-01-27 14:50:39 +0000

    Refresh config file structure and added smhi to it.
    /Joakim

--- scrapWind.py
+++ scrapWind.py
@@ -15,10 +15,10 @@ def loadConfig(filename='config.yaml'):
 def scrapeDataDog(config, update=False):
     datan = []
     payload = {}
-    kollaDessaClasser = {'storhetClassName': 'Kol_tx_storhet', 'medelClassName': 'Kol_tx_medel', 'minClassName': 'Kol_tx_mi', 'maxClassName': 'Kol_tx_ma'}
-    typeName = {'Kol_tx_medel': '_medel', 'Kol_tx_mi': '_min', 'Kol_tx_ma': '_max'}
+#    kollaDessaClasser = {'storhetClassName': 'Kol_tx_storhet', 'medelClassName': 'Kol_tx_medel', 'minClassName': 'Kol_tx_mi', 'maxClassName': 'Kol_tx_ma'}
+#    typeName = {'Kol_tx_medel': '_medel', 'Kol_tx_mi': '_min', 'Kol_tx_ma': '_max'}
 
-    response = requests.get(config['borstahusenspir_url'])
+    response = requests.get(config['borstahusenspir']['url'])
     timestamp = int(time.time())*1000 # Milliseconds since 1970
 
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -31,7 +31,7 @@ def scrapeDataDog(config, update=False):
     allRows = soup.find_all('tr')
     for row in range(len(allRows)):
         datan.append({})
-        for key, value in kollaDessaClasser.iteritems():
+        for key, value in config['borstahusenspir']['kollaDessaClasser'].iteritems():
             if key == 'storhetClassName':
                 allColumns = allRows[row].find_all('td', class_=value)
                 for column in range(len(allColumns)):
@@ -44,8 +44,8 @@ def scrapeDataDog(config, update=False):
                         datan[row]['enhet'] = allColumns[column].get_text().split()[1]
 
     for unit in datan:
-        if kollaDessaClasser['storhetClassName'] in unit:
-            for t, n in typeName.iteritems():
+        if config['borstahusenspir']['kollaDessaClasser']['storhetClassName'] in unit:
+            for t, n in config['borstahusenspir']['typeName'].iteritems():
                 try:
                     payload[unit['Kol_tx_storhet'].replace(' ','_')+n] = {'value': unit[t], 'timestamp': timestamp, 'context': {'lat': 55.894468, 'lng': 12.799568}}
                 except (KeyError) as e:
@@ -53,7 +53,7 @@ def scrapeDataDog(config, update=False):
 #    print json.dumps(payload)
     if update:
         if len(payload):
-            rc = requests.post(config['ubidots_urlprefix']+'/api/v1.6/devices/'+config['ubidots_source']+'/?token='+config['ubidots_token'], headers={'Content-Type': 'application/json'}, data=json.dumps(payload))
+            rc = requests.post(config['ubidots']['urlprefix']+'/api/v1.6/devices/'+config['ubidots']['datadog_source']+'/?token='+config['ubidots']['token'], headers={'Content-Type': 'application/json'}, data=json.dumps(payload))
 #            print rc
             print rc.content
         else:
@@ -64,13 +64,12 @@ def scrapeDataDog(config, update=False):
 ################################################################
 
 def scrapeSMHI(config, update=False):
-#    seaLevelLocations = {'Barseback': 2099, 'Viken': 2228, 'Klagshamn': 2228, 'Skanor': 30488, 'Ystad': 2093}
-    seaLevelLocations = {'Barseback': 2099, 'Viken': 2228, 'Klagshamn': 2095, 'Skanor': 30488}
+#    seaLevelLocations = {'Barseback': 2099, 'Viken': 2228, 'Klagshamn': 2095, 'Skanor': 30488}
     vattenPayload = {}
-    for key, value in seaLevelLocations.iteritems():
+    for key, value in config['smhi']['seaLevelLocations'].iteritems():
 
         headers = {'Content-type': 'application/json'}
-        url = "http://opendata-download-ocobs.smhi.se/api/version/latest/parameter/6/station/"+str(value)+"/period/latest-hour/data.json"
+        url = config['smhi']['urlprefix']+str(value)+"/period/latest-hour/data.json"
         try:
             response = requests.get(url, headers=headers)
             responseData = response.json()
@@ -81,7 +80,7 @@ def scrapeSMHI(config, update=False):
 #    print json.dumps(vattenPayload)
     if update:
         if len(vattenPayload) != 0:
-            rc = requests.post(config['ubidots_urlprefix']+'/api/v1.6/devices/'+'smhi'+'/?token='+config['ubidots_token'], headers={'Content-Type': 'application/json'}, data=json.dumps(vattenPayload))
+            rc = requests.post(config['ubidots']['urlprefix']+'/api/v1.6/devices/'+config['ubidots']['smhi_source']+'/?token='+config['ubidots']['token'], headers={'Content-Type': 'application/json'}, data=json.dumps(vattenPayload))
 #            print rc
             print rc.content
         else:
@@ -93,8 +92,10 @@ def scrapeSMHI(config, update=False):
 
 def lambda_handler(event, context):
     config = loadConfig(filename='config.yaml')
-    scrapeDataDog(config, update=config['ubidots_update'])
-    scrapeSMHI(config, update=config['ubidots_update'])
+    print json.dumps(config)
+#    exit()
+    scrapeDataDog(config, update=config['ubidots']['update'])
+    scrapeSMHI(config, update=config['ubidots']['update'])
 
 if __name__ == '__main__':
     lambda_handler(False, False)
